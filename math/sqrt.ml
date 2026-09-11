10e41906f05d3fedcfa0112e193cb0a3313a3480
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-09-10 21:25:26 -0400

    Notes

--- math/sqrt.ml
+++ math/sqrt.ml
@@ -3,7 +3,7 @@ let n = float_of_string Sys.argv.(1);;
 let rec sqrt n x =
        
         let n1 = (x +. (n /. x)) /. 2. in
-        if abs_float(n1 -. x) < 0.1 then n1 else
+        if abs_float(n1 -. x) < 0.0001 then n1 else
                sqrt n n1
         
 ;;
