ce3b70a9e7948572bfeaaaad27f496e35898bfaf
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-09-21 04:33:04 -0400

    Functions

--- math/fun.ml
+++ math/fun.ml
@@ -1,7 +1,20 @@
+let x = float_of_string Sys.argv.(1)
 type exp = 
 |C of float
 |X
-|P
-|T
-|D
+|P of exp * exp
+|T of exp * exp
+|D of exp * exp
 
+let rec eval (e : exp) (x : float) : float = 
+        match e with 
+        |X -> x
+        |C(c) -> c
+        |P(a, b)-> eval a x +. eval b x
+        |T(a,b)-> eval a x *. eval b x
+        |D(a,b)-> eval a x /. eval b x
+;;
+let e = P(P(T(X, X), T(C(2.),X)), C(1.))
+let r = eval e x
+let () =
+        print_float(r);;
